47fcd43433aad72a12bccd0959bda9150afc17a3
Josh Nichols <josh.nichols.lives@gmail.com>
2018-02-26 17:36:21 -0700

    detecting changes in input

--- main.ino
+++ main.ino
@@ -1,15 +1,25 @@
 int val = 0;
+int prevVal = 0;
+
 int pin = A0;
+int outPin = 2;
 void setup() {
+  pinMode(outPin, OUTPUT);
   Serial.begin(9600);
 }
+
 void loop() {
   // reply only when you receive data:
   // read the incoming byte:
   val = analogRead(pin);
-
-  // say what you got:
-  Serial.print("I received: ");
-  Serial.println(val, DEC);
-  delay(600);
+  if (val - prevVal > 0){
+    digitalWrite(outPin, HIGH);
+  }
+  else {
+    digitalWrite(outPin, LOW);
+  }
+  //  // say what you got:
+  // Serial.print("I received: ");
+  // Serial.println(val, DEC);
+  // delay(600);
 }
