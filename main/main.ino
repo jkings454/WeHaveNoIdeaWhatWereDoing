96638a87b1e13e46e11edc03969a7cf086ce0a96
Josh Nichols <josh.nichols.lives@gmail.com>
2018-04-14 16:34:21 -0600

    Should work with N * N grid

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,10 @@
 #include <FFT.h>
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, 6, NEO_GRB + NEO_KHZ800);
+#define HEIGHT 10
+#define WIDTH 10
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(HEIGHT * WIDTH, 6, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
@@ -39,7 +42,7 @@ void loop()
   fft_mag_octave(); // take the output of the fft
   sei();
 
-  int ledOut[8];
+  int ledOut[WIDTH];
   int average = 0;
 
   for (int i = 0; i < 8; i++)
@@ -47,13 +50,13 @@ void loop()
     ledOut[i] = constrain(fft_oct_out[i] / 20, 0, 9);
     average = average + ledOut[i];
   }
-
-  average = constrain(average / 8, 0, 9);
-  for (int i = 0; i < ledOut[0]; i++)
-  {
-    strip.setPixelColor(i, strip.Color(150, 0, 0));
+  // in order to get the nth led of the zth column:
+  // x = (Number of Rows * z) + n;
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = i * HEIGHT; j < (i * HEIGHT) + ledOut[i]; j++) {
+      strip.setPixelColor(j, 127, 0, 0);
+    }
   }
-  Serial.println(ledOut[0]);
   strip.show();
   delay(1000);
 }
