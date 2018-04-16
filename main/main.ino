52663098f65731e0717656211c6c2b12ae827f21
Josh Nichols <josh.nichols.lives@gmail.com>
2018-04-16 12:52:25 -0600

    finalized code

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,38 @@
 #define OCTAVE 1
+
+#include <Wire.h>
 #include <FFT.h>
 #include <Adafruit_NeoPixel.h>
+#include "Adafruit_TCS34725.h"
 
 #define HEIGHT 10
 #define WIDTH 10
+#define LED_PIN 6
+#define LIGHT_PIN 5
+#define DELAY 1000
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(HEIGHT * WIDTH, 6, NEO_GRB + NEO_KHZ800);
+#define NOISE_REDUCTION 30
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(HEIGHT * WIDTH, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+int iter = 0;
+int rSum = 0;
+int gSum = 0;
+int bSum = 0;
+
+uint16_t clear, red, green, blue;
+
+int redOut;
+int greenOut;
+int blueOut;
+int d = 55;
 
 void setup()
 {
   Serial.begin(115200); // use the serial port
   strip.begin();
+  tcs.begin();
+
   TIMSK0 = 0;    // turn off timer0 for lower jitter
   ADCSRA = 0xe5; // set the adc to free running mode
   ADMUX = 0x40;  // use adc0
@@ -19,9 +41,18 @@ void setup()
 
 void loop()
 {
-  for (int i = 0; i < 8; i++) {
+  tcs.setInterrupt(false); 
+  delay(d);
+  tcs.getRawData(&red, &green, &blue, &clear);
+  int upperBound = 1500;
+  redOut = constrain(map(red, 0, upperBound, 0, 255), 0, 255);
+  greenOut = constrain(map(green, 0, upperBound, 0, 255), 0, 255);
+  blueOut = constrain(map(blue, 0, upperBound, 0, 255), 0, 255);
+
+  for (int i = 0; i < HEIGHT * WIDTH; i++) {
     strip.setPixelColor(i, 0, 0, 0);
   }
+  
   cli(); // UDRE interrupt slows this way down on arduino1.0
   for (int i = 0; i < 512; i += 2)
   { // save 256 samples
@@ -47,16 +78,25 @@ void loop()
 
   for (int i = 0; i < 8; i++)
   {
-    ledOut[i] = constrain(fft_oct_out[i] / 20, 0, 9);
+    ledOut[i + 1] = constrain((fft_oct_out[i] - NOISE_REDUCTION) / 20, 0, 9);
     average = average + ledOut[i];
+    Serial.print(fft_oct_out[i]);
+    Serial.print(" ");
   }
+
+  average = average / 8;
+
+  ledOut[0] = average;
+  ledOut[WIDTH - 1] = average;
+
+  Serial.println();
   // in order to get the nth led of the zth column:
   // x = (Number of Rows * z) + n;
   for (int i = 0; i < WIDTH; i++) {
     for (int j = i * HEIGHT; j < (i * HEIGHT) + ledOut[i]; j++) {
-      strip.setPixelColor(j, 127, 0, 0);
+      strip.setPixelColor(j, redOut, blueOut, greenOut);
     }
   }
   strip.show();
-  delay(1000);
+  delay(DELAY);
 }
---
+++ visualizer.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import serial
+import sys
+import re
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=100)
+
+def get_data():
+    line = ser.readline()
+    line = line.decode('utf8')
+    re.sub(r'^[0-9 ]', "", line)
+    line.strip()
+    arr = line.split()
+    arr = list(map(int, arr))
+    return arr
+
+def update(frame):
+    # frame is irrelavent but required
+    plt.cla()
+    arr = get_data()
+    while len(arr) < 8:
+        arr = get_data()
+        
+    x = plt.bar(range(8), arr, 1)
+    return x
+
+fig = plt.figure()
+d = get_data()
+dummy = [0, 3, 5, 6, 2, 1, 8, 20]
+bar = plt.bar(range(8), dummy, 1)
+
+ani = animation.FuncAnimation(fig, update, frames=60, interval=70, blit=True)
+plt.show()
